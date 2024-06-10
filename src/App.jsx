6c88f5dcc9f9ebf48abf92d00f11f07aa4e35b00
Stephen Werronen <stephen.werronen@gmail.com>
2024-06-10 15:51:50 -0700

    Update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.scss";
 
 import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
