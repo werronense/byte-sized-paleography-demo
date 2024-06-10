7feb6d2e9092dea981c40896d04840f2727fe681
Stephen Werronen <stephen.werronen@gmail.com>
2024-06-10 16:37:19 -0700

    Copy Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.scss";
 
+import Header from "./components/Header/Header";
+
 import LandingPage from "./pages/LandingPage/LandingPage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="*" element={<ErrorPage />} />
